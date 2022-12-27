934cb05144174300ff2f14e0084f6a62a7dd4d16
Anish Kasam <anishkasam@gmail.com>
2022-12-27 12:07:25 -0800

    optimize code

---
+++ 2019.py
@@ -0,0 +1,113 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+yearlydata = pd.read_csv("2019data/2019yearly.csv")
+defenses = pd.read_csv("2019data/2019defenses.csv")
+schedules = pd.read_csv("2019data/2019schedule.csv")
+
+yearlydata = yearlydata[yearlydata.get("Pos").isin(["QB", "RB", "WR", "TE"])]
+yearlydata = yearlydata[yearlydata.get("G") >= 2]
+yearlydata = yearlydata.drop(["Age", "GS", "Tgt", "Int", "Fumbles", "FumblesLost", "ReceivingYds", "ReceivingTD", "RushingYds", "RushingTD", "PassingYds", "PassingTD", "PassingAtt", "RushingAtt", "Rec"], axis = 1)
+yearlydata["AvgFantasyPoints"] = yearlydata.get("FantasyPoints") / yearlydata.get("G")
+yearlydata = yearlydata.rename(columns = {"Tm": "Team"})
+yearlydata = yearlydata.sort_values(by = "Player")
+yearlydata = yearlydata.set_index("Player")
+
+for i in range(1, 17):
+  weeklydata = pd.read_csv("2019data/2019weekly/week" + str(i) + ".csv")
+  weeklydata = weeklydata[weeklydata.get("Pos").isin(["QB", "RB", "WR", "TE"])]
+  weeklydata = weeklydata.rename(columns = {"PPRFantasyPoints": "Week " + str(i)})
+  weeklydata = weeklydata.drop(["StandardFantasyPoints", "HalfPPRFantasyPoints", "ReceivingYds", "ReceivingTD", "RushingYds", "RushingTD", "PassingYds", "PassingTD", "PassingAtt", "RushingAtt", "Rec", "Int", "Cmp", "Tgt", "FL", "Tm", "Pos"], axis = 1)
+  weeklydata = weeklydata.sort_values(by = "Player")
+  weeklydata = weeklydata.set_index("Player")
+
+  if i == 1:
+    data = pd.merge(yearlydata, weeklydata, left_index = True, right_index = True)
+  else:
+    data = data.merge(weeklydata, left_index = True, right_index = True, how = "left")
+  
+schedules = schedules.drop(["W17"], axis = 1)
+
+def removeAway(x):
+  x = x.replace("@", "")
+  x = x.replace("GB", "GNB")
+  x = x.replace("LV", "LVR")
+  x = x.replace("TB", "TAM")
+  x = x.replace("KC", "KAN")
+  x = x.replace("NE", "NWE")
+  return x
+
+def correlation(player):
+  try: 
+    pointdiffs = []
+    rankings = []
+    team = data.loc[player].get("Team")
+    pos = data.loc[player].get("Pos")
+
+    for i in range(1, 17):
+      points = data.loc[player].get("Week " + str(i))
+      opponent = schedules.loc[team].get("W" + str(i))
+
+      if (opponent == "BYE" or opponent == "OAK" or points == "NaN"):
+        continue
+
+      pointdiff = points - data.loc[player].get("AvgFantasyPoints")
+      ranking = defenses.loc[opponent].get(pos + " Rank")
+
+      pointdiffs.append(round(pointdiff, 2))
+      rankings.append(ranking)
+    
+    x = pd.Series(rankings)
+    y = pd.Series(pointdiffs)
+
+    return round(x.corr(y), 2)
+  except:
+    return 0
+def graph(player):
+  pointdiffs = []
+  rankings = []
+  team = data.loc[player].get("Team")
+  pos = data.loc[player].get("Pos")
+
+  for i in range(1, 17):
+    points = data.loc[player].get("Week " + str(i))
+    opponent = schedules.loc[team].get("W" + str(i))
+
+    if (opponent == "BYE" or opponent == "OAK" or points == "NaN"):
+      continue
+
+    pointdiff = points - data.loc[player].get("AvgFantasyPoints")
+    ranking = defenses.loc[opponent].get(pos + " Rank")
+
+    pointdiffs.append(round(pointdiff, 2))
+    rankings.append(ranking)
+  
+  x = pd.Series(rankings)
+  y = pd.Series(pointdiffs)
+
+  plt.scatter(x, y)
+  plt.title("Effect of Defense Strength on " + str(player) + " in 2017")
+  plt.xlabel("Defense Ranking (1-32) | Correlation = " + str(correlation(player)))
+  plt.ylabel("Fantasy Production Above/Below Yearly Mean")
+  xdata = np.array(x)
+  ydata = np.array(y)
+  m, b = np.polyfit(xdata, ydata, 1)
+  plt.plot(x, m*x + b)
+  plt.show()
+
+schedules["Team"] = schedules["Team"].apply(removeAway)
+schedules = schedules.set_index("Team")
+for i in range(1,17):
+  schedules["W" + str(i)] = schedules["W" + str(i)].apply(removeAway)
+
+defenses = defenses.set_index("Team")
+
+data = data.assign(Correlation = data.index.map(correlation))
+data = data.assign(AbsCorrelation = data.get("Correlation").apply(abs))
+sigdata = data[data.get("AvgFantasyPoints") > 6].sort_values("AbsCorrelation", ascending = False)
+sigdata = sigdata[sigdata.get("AbsCorrelation") > 0.6]
+sigdata = sigdata[sigdata.get("G") > 6]
+
+print(data)
+print(sigdata)
---
+++ rankings.py
@@ -0,0 +1,97 @@
+# cleans ranking data from online (https://www.fantasypros.com/nfl/points-allowed.php?year=2018)
+
+import pandas as pd
+import csv
+
+
+file = "2019data/2019defenses.csv"
+rankings = pd.read_csv(file)
+
+teamprofiles = []
+
+with open(file, "r") as f: 
+  reader = csv.reader(f)
+
+  teams = len(list(reader)) - 2
+  for i in range(teams):
+    teamname = rankings["Team"][i]
+
+    if teamname == "Arizona Cardinals":
+      teamname = "ARI"
+    elif teamname == "Atlanta Falcons":
+      teamname = "ATL"
+    elif teamname == "Baltimore Ravens":
+      teamname = "BAL"
+    elif teamname == "Buffalo Bills":
+      teamname = "BUF"
+    elif teamname == "Carolina Panthers":
+      teamname = "CAR"
+    elif teamname == "Chicago Bears":
+      teamname = "CHI"
+    elif teamname == "Cincinnati Bengals":
+      teamname = "CIN"
+    elif teamname == "Cleveland Browns":
+      teamname = "CLE"
+    elif teamname == "Dallas Cowboys":
+      teamname = "DAL"
+    elif teamname == "Denver Broncos":
+      teamname = "DEN"
+    elif teamname == "Detroit Lions":
+      teamname = "DET"
+    elif teamname == "Green Bay Packers":
+      teamname = "GNB"
+    elif teamname == "Houston Texans":
+      teamname = "HOU"
+    elif teamname == "Indianapolis Colts":
+      teamname = "IND"
+    elif teamname == "Jacksonville Jaguars":
+      teamname = "JAX"
+    elif teamname == "Kansas City Chiefs":
+      teamname = "KAN"
+    elif teamname == "Las Vegas Raiders":
+      teamname = "OAK"
+    elif teamname == "Los Angeles Chargers":
+      teamname = "LAC"
+    elif teamname == "Los Angeles Rams":
+      teamname = "LAR"
+    elif teamname == "Miami Dolphins":
+      teamname = "MIA"
+    elif teamname == "Minnesota Vikings":
+      teamname = "MIN"
+    elif teamname == "New England Patriots":
+      teamname = "NWE"
+    elif teamname == "New Orleans Saints":
+      teamname = "NOR"
+    elif teamname == "New York Giants":
+      teamname = "NYG"
+    elif teamname == "New York Jets":
+      teamname = "NYJ"
+    elif teamname == "Philadelphia Eagles":
+      teamname = "PHI"
+    elif teamname == "Pittsburgh Steelers":
+      teamname = "PIT"
+    elif teamname == "San Francisco 49ers":
+      teamname = "SFO"
+    elif teamname == "Seattle Seahawks":
+      teamname = "SEA"
+    elif teamname == "Tampa Bay Buccaneers":
+      teamname = "TAM"
+    elif teamname == "Tennessee Titans":
+      teamname = "TEN"
+    elif teamname == "Washington Commanders":
+      teamname = "WAS"
+
+    qbrank = 33 - rankings["Rank1"][i]
+    rbrank = 33 - rankings["Rank2"][i]
+    wrrank = 33 - rankings["Rank3"][i]
+    terank = 33 - rankings["Rank4"][i]
+
+    teamprofile = [teamname, qbrank, rbrank, wrrank, terank]
+    teamprofiles.append(teamprofile)
+
+with open(file, "w") as f:
+  writer = csv.writer(f)
+
+  headers = ["Team", "QB Rank", "RB Rank", "WR Rank", "TE Rank"]
+  writer.writerow(headers)
+  writer.writerows(teamprofiles)
